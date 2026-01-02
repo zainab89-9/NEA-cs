be83a4add3188624e368bbcc4d9d711f478db928
Zainab <zainab._.x@icloud.com>
2026-01-02 17:32:47 +0000

    created the basic GUI window

--- python.py
+++ python.py
@@ -1,3 +1,9 @@
 from tkinter import * #Python GUI platform
 import customtkinter #Extention of tkinter
 import pygame #Python game library
+
+root = Tk() #Creates the main window for the GUI
+
+root.geometry("500x600") #Size of the window
+
+root.mainloop() #Creates the window and waits for the user to interact with it
